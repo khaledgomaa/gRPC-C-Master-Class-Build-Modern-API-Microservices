4137214c83ce517f0613c46098e446843b0e5465
Khaled Gomaa <khaledgomaa670@gmail.com>
2024-05-31 14:43:27 +0300

    Added tasks

---
+++ Grpc.Client/Program.cs
@@ -0,0 +1,84 @@
+﻿using Grpc.Net.Client;
+using HelloWorld;
+
+await BiDirectionalClientCalculator();
+
+async static Task BiDirectionalClientCalculator()
+{
+    using var channel = GrpcChannel.ForAddress("http://localhost:50051");
+    var client = new Calculator.CalculatorClient(channel);
+
+    var stream = client.FindMaximum();
+
+    var responseResult = Task.Run(async () =>
+    {
+        while (await stream.ResponseStream.MoveNext(CancellationToken.None))
+        {
+            Console.WriteLine("Current max: {0}", stream.ResponseStream.Current.Result);
+        }
+    });
+
+    for (int i = 1; i <= 10; i++)
+    {
+        await stream.RequestStream.WriteAsync(new() { Number = i });
+    }
+
+    await stream.RequestStream.CompleteAsync();
+    await responseResult;
+
+    Console.WriteLine("Press any key to exit...");
+    Console.ReadKey();
+}
+
+async static Task StreamClientCalculator()
+{
+    using var channel = GrpcChannel.ForAddress("http://localhost:50051");
+    var client = new Calculator.CalculatorClient(channel);
+
+    var stream = client.ComputeAverage();
+
+    for (int i = 1; i <= 10; i++)
+    {
+        await stream.RequestStream.WriteAsync(new() { Number = i });
+    }
+
+    await stream.RequestStream.CompleteAsync();
+
+    var resposne = await stream.ResponseAsync.WaitAsync(CancellationToken.None);
+
+    Console.WriteLine("Average: {0}", resposne.Result);
+
+    Console.WriteLine("Press any key to exit...");
+    Console.ReadKey();
+}
+
+async static Task UnaryClientCalculator()
+{
+    using var channel = GrpcChannel.ForAddress("http://localhost:50051");
+    var client = new Calculator.CalculatorClient(channel);
+
+    var reply = client.GetPimeDecomposition(new() { Number = 120 });
+
+    while (await reply.ResponseStream.MoveNext(CancellationToken.None))
+    {
+        Console.WriteLine(reply.ResponseStream.Current.Result);
+    }
+
+    await channel.ShutdownAsync().WaitAsync(CancellationToken.None);
+
+    Console.WriteLine("Press any key to exit...");
+    Console.ReadKey();
+}
+
+async static Task UnaryClient()
+{
+    using var channel = GrpcChannel.ForAddress("http://localhost:50051");
+    var client = new Greeter.GreeterClient(channel);
+
+    // Call the SayHello method
+    var reply = await client.SayHelloAsync(new HelloRequest { Name = "Khaled" });
+
+    Console.WriteLine("Greeting: " + reply.Message);
+    Console.WriteLine("Press any key to exit...");
+    Console.ReadKey();
+}
---
+++ Grpc.Server/Program.cs
@@ -0,0 +1,113 @@
+﻿using Grpc.Core;
+using HelloWorld;
+
+const int Port = 50051;
+
+ServerBIDirectionalExample(Port);
+
+static void UnaryExample(int Port)
+{
+    Server server = new()
+    {
+        Services = { Greeter.BindService(new GreeterImpl()) },
+        Ports = { new ServerPort("localhost", Port, ServerCredentials.Insecure) }
+    };
+    server.Start();
+
+    Console.WriteLine("Greeter server listening on port " + Port);
+    Console.WriteLine("Press any key to stop the server...");
+    Console.ReadKey();
+
+    server.ShutdownAsync().Wait();
+}
+
+static void ServerStreamExample(int Port)
+{
+    Server server = new()
+    {
+        Services = { Calculator.BindService(new CalculatorImp()) },
+        Ports = { new ServerPort("localhost", Port, ServerCredentials.Insecure) }
+    };
+    server.Start();
+
+    Console.WriteLine("Calculator server listening on port " + Port);
+    Console.WriteLine("Press any key to stop the server...");
+    Console.ReadKey();
+
+    server.ShutdownAsync().Wait();
+}
+
+static void ServerBIDirectionalExample(int Port)
+{
+    Server server = new()
+    {
+        Services = { Calculator.BindService(new CalculatorImp()) },
+        Ports = { new ServerPort("localhost", Port, ServerCredentials.Insecure) }
+    };
+    server.Start();
+
+    Console.WriteLine("Calculator server listening on port " + Port);
+    Console.WriteLine("Press any key to stop the server...");
+    Console.ReadKey();
+
+    server.ShutdownAsync().Wait();
+}
+
+class GreeterImpl : Greeter.GreeterBase
+{
+    public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
+    {
+        return Task.FromResult(new HelloReply { Message = $"Hello, {request.Name} from RPC server" });
+    }
+}
+
+class CalculatorImp : Calculator.CalculatorBase
+{
+    public async override Task GetPimeDecomposition(PrimeDecompositionRequest request, IServerStreamWriter<PrimeDecompositionResult> responseStream, ServerCallContext context)
+    {
+        int k = 2;
+        while (request.Number > 1)
+        {
+            if (request.Number % k == 0)
+            {
+                await responseStream.WriteAsync(new() { Result = k });
+                request.Number /= k;
+            }
+            else
+            {
+                k++;
+            }
+
+        }
+    }
+
+    public async override Task<CalculateAverageResponse> ComputeAverage(IAsyncStreamReader<CalculateAverageRequest> requestStream, ServerCallContext context)
+    {
+        int count = 0;
+        double sum = 0;
+
+        while (await requestStream.MoveNext())
+        {
+            count++;
+            sum += requestStream.Current.Number;
+        }
+
+        return new() { Result = sum / count };
+    }
+
+    public override async Task FindMaximum(IAsyncStreamReader<FindMaximumRequest> requestStream, IServerStreamWriter<FindMaximumResponse> responseStream, ServerCallContext context)
+    {
+        int max = int.MinValue;
+
+        while (await requestStream.MoveNext())
+        {
+            if (requestStream.Current.Number > max)
+            {
+                max = requestStream.Current.Number;
+
+                await responseStream.WriteAsync(new() { Result = max });
+            }
+        }
+
+    }
+}
